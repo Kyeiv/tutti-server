d164359bb00845e35e3a2a592fbfc8ee8bb25736
Kyeiv <krzy.poloczek@gmail.com>
2019-07-28 20:45:43 +0200

    enabled auth

--- src/main/java/pl/com/tutti/tuttiserver/TuttiServerApplication.java
+++ src/main/java/pl/com/tutti/tuttiserver/TuttiServerApplication.java
@@ -2,11 +2,16 @@ package pl.com.tutti.tuttiserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import pl.com.tutti.tuttiserver.config.SecurityConfig;
 
 @SpringBootApplication
 public class TuttiServerApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TuttiServerApplication.class, args);
 	}
--- src/main/java/pl/com/tutti/tuttiserver/config/SecurityConfig.java
+++ src/main/java/pl/com/tutti/tuttiserver/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package pl.com.tutti.tuttiserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.session.SessionManagementFilter;
 
+import pl.com.tutti.tuttiserver.config.webfilter.CorsFilter;
 import pl.com.tutti.tuttiserver.config.webfilter.CsrfTokenResponseHeaderBindingFilter;
 import pl.com.tutti.tuttiserver.rest.authentication.SimpleAuthenticationEntryPoint;
 import pl.com.tutti.tuttiserver.rest.authentication.SimpleAuthenticationFailureHandler;
@@ -27,23 +33,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		auth.inMemoryAuthentication().withUser("user").password("user").roles("USER").and().withUser("admin")
-		.password("admin").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER").and().withUser("admin")
+		.password("{noop}admin").roles("ADMIN");
 	}
+	
+	@Bean
+    CorsFilter corsFilter() {
+        CorsFilter filter = new CorsFilter();
+        return filter;
+    }
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-	    http.authorizeRequests() 
+	    http
+	    .addFilterBefore(corsFilter(), SessionManagementFilter.class) 
+	    .authorizeRequests() 
+	    .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 	    .antMatchers("/auth/**").authenticated()
-	    .antMatchers("/api/**").authenticated();
-	    http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
-		http.formLogin().successHandler(authenticationSuccessHandler);
-		http.formLogin().failureHandler(authenticationFailureHandler);
-		http.logout().logoutSuccessUrl("/");
-		
-	    // CSRF tokens handling
-	    http.addFilterAfter(new CsrfTokenResponseHeaderBindingFilter(), CsrfFilter.class);
+	    .antMatchers("/api/**").authenticated()
+	    .and()
+	    .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+	    .and()
+		.formLogin()
+		.successHandler(authenticationSuccessHandler)
+		.failureHandler(authenticationFailureHandler)
+		.and()
+		.logout().logoutSuccessUrl("/")
+	    .and()
+	    .csrf().disable()
+		.addFilterAfter(new CsrfTokenResponseHeaderBindingFilter(), CsrfFilter.class);	
 	}
 
 }
