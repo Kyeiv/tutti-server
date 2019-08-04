0bbeeb30dc12606d8727e8d6e88d59f111ced3f9
Kyeiv <krzy.poloczek@gmail.com>
2019-08-04 18:35:31 +0200

    added configure adapter

--- src/main/java/pl/com/tutti/tuttiserver/config/SecurityConfig.java
+++ src/main/java/pl/com/tutti/tuttiserver/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package pl.com.tutti.tuttiserver.config;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +11,11 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.session.SessionManagementFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import pl.com.tutti.tuttiserver.config.webfilter.CorsFilter;
 import pl.com.tutti.tuttiserver.config.webfilter.CsrfTokenResponseHeaderBindingFilter;
 import pl.com.tutti.tuttiserver.rest.authentication.SimpleAuthenticationEntryPoint;
 import pl.com.tutti.tuttiserver.rest.authentication.SimpleAuthenticationFailureHandler;
@@ -37,19 +39,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.password("{noop}admin").roles("ADMIN");
 	}
 	
+//	@Bean
+//    CorsFilter corsFilter() {
+//        CorsFilter filter = new CorsFilter();
+//        return filter;
+//    }
+	
 	@Bean
-    CorsFilter corsFilter() {
-        CorsFilter filter = new CorsFilter();
-        return filter;
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(new ArrayList<String>(Arrays.asList("*")));
+        configuration.setAllowedMethods(new ArrayList<String>(Arrays.asList("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH")));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(new ArrayList<String>(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "access-control-allow-origin")));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
 	    http
-	    .addFilterBefore(corsFilter(), SessionManagementFilter.class) 
+	    .cors().and()
+//	    .addFilterBefore(corsFilter(), SessionManagementFilter.class)
 	    .authorizeRequests() 
-	    .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+	    //.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 	    .antMatchers("/auth/**").authenticated()
 	    .antMatchers("/api/**").authenticated()
 	    .and()
