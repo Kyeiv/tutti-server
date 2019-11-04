d106791046740702b6f4d9edb8137973112f99ad
Kyeiv <krzy.poloczek@gmail.com>
2019-11-04 19:39:25 +0100

    search users hack

--- src/main/java/pl/com/tutti/tuttiserver/rest/controller/UsersRestController.java
+++ src/main/java/pl/com/tutti/tuttiserver/rest/controller/UsersRestController.java
@@ -5,10 +5,8 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.com.tutti.tuttiserver.entity.Specialization;
 import pl.com.tutti.tuttiserver.entity.Users;
 import pl.com.tutti.tuttiserver.rest.controller.utils.ResponseFactory;
 import pl.com.tutti.tuttiserver.rest.data.SearchTeachersData;
@@ -23,9 +21,16 @@ public class UsersRestController {
 	
 	private UsersService usersService;
 
-	@GetMapping("/users/search")
-	public ResponseEntity searchByCitySpecLevelSpecName(@Valid @RequestBody SearchTeachersData searchTeachersData, Principal principal){
+	@PostMapping("/users/search")
+	public ResponseEntity searchByCitySpecLevelSpecName(@Valid @RequestBody SearchTeachersData searchTeachersData){
 		List<Users> users = usersService.findByCityAndSpecNameAndSpecLevel(searchTeachersData);
+
+		for(Users user: users){
+			user.setSpecializations(null);
+			user.setAvailbilities(null);
+			user.setPosts(null);
+			user.setAppointmentsAsTutor(null);
+		}
 
 		return ResponseFactory.createPayloadResponse(users, "searchByCitySpecLevelSpecName");
 	}
