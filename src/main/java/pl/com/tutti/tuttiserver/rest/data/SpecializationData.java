637cc5a3b96937ea92ef1a67b3a18954ec868452
Kyeiv <krzy.poloczek@gmail.com>
2019-12-27 16:45:27 +0100

    added small constrains to data

--- src/main/java/pl/com/tutti/tuttiserver/rest/data/SpecializationData.java
+++ src/main/java/pl/com/tutti/tuttiserver/rest/data/SpecializationData.java
@@ -5,13 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SpecializationData {
+    @NotNull
     private String level;
+    @NotNull
     private String name;
+    @NotNull
     private Float salary;
     private Integer id;
 }
